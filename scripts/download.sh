b14d931491d53a0f86afbc948f70bfbc40803679
Lauranav13 <lauranavarrohdez@hotmail.com>
2022-11-17 20:49:32 +0100

    merged files, cutadapt and STAR

--- scripts/download.sh
+++ scripts/download.sh
@@ -1,8 +1,11 @@
+echo "Downloading samples..."
 
 	url=$1
 	sample=$(basename $1)
 
 wget -P $2 $1
+
+echo "Uncompressing samples..."
 
 if [ "$3" = "yes" ]
 then
--- scripts/pipeline.sh
+++ scripts/pipeline.sh
@@ -8,3 +8,13 @@ bash scripts/download.sh https://bioinformatics.cnio.es/data/courses/decont/cont
 
 bash scripts/index.sh res/contaminants.fasta res/contaminants_idx
 
+for sid in $(cat data/urls | cut -d"/" -f7 | cut -d"_" -f1,2 | cut -d"-" -f1 | sort | uniq)
+do
+    bash scripts/merge_fastqs.sh data out/merged $sid
+done
+
+for sample in $(cat data/urls | cut -d"/" -f7 | cut -d"_" -f1,2 | cut -d"-" -f1 | sort | uniq)
+do
+    bash scripts/analyse.sh $sample
+done
+
